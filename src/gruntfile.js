b592f1bcec9e21f6a791e3d47094f7dc2bbaf6ee
Sergey <anisimov.sergey@gmail.com>
2015-08-19 21:06:57 +0100

    Adding rename grunt step.

--- src/gruntfile.js
+++ src/gruntfile.js
@@ -7,9 +7,9 @@ module.exports = function(grunt) {
                     archive: 'TeamCityGadget.zip'
                 },
                 files: [
-                    {src: ['css/**'], dest: 'css/'},
-                    {src: ['images/**'], dest: 'images/'},
-                    {src: ['scripts/**'], dest: 'scripts/'},
+                    {src: ['css/**'], dest: '/'},
+                    {src: ['images/**'], dest: '/'},
+                    {src: ['scripts/**'], dest: '/'},
                     {src: ['drag.png'], dest: '/'},
                     {src: ['flyout_details.html'], dest: '/'},
                     {src: ['gadget.html'], dest: '/'},
@@ -17,24 +17,18 @@ module.exports = function(grunt) {
                     {src: ['icon.png'], dest: '/'},
                     {src: ['settings.html'], dest: '/'}
                 ]
-            },
-            rename: {
-                moveThis: {
-                    src: 'test/this',
-                    dest: 'output/'
-                },
-
-                // Any number of targets here...
-
-                moveThat: {
-                    src: 'test/that',
-                    dest: 'output/that'
-                }
+            }
+        },
+        rename: {
+            moveThis: {
+                src: 'TeamCityGadget.zip',
+                dest: '../output/TeamCityGadget.gadget'
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-rename');
 
-    grunt.registerTask('default', ['compress']);
+    grunt.registerTask('default', ['compress','rename']);
 };
